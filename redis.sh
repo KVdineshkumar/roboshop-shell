ade80c6b05251818dd30efbf087f101af703a47e
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-01 22:33:36 +0530

    catalog cogi

---
+++ redis.sh
@@ -0,0 +1,17 @@
+echo -e "\c[31mInstalling redis repo/e[0m"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
+yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
+
+echo -e "\c[31mInstalling redis /e[0m"
+yum install redis -y &>>/tmp/roboshop.log
+
+echo -e "\c[31mUpdating listing Address /e[0m"
+sed -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>/tmp/roboshop.log
+
+echo -e "\c[31mStarting Redis /e[0m"
+systemctl enable redis  &>>/tmp/roboshop.log
+systemctl start redis  &>>/tmp/roboshop.log
+
+
+
+
