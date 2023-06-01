a674fd1d6a0eb0aa0f6c6c1cd2eb1bc9efa196ce
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-02 00:00:29 +0530

    user

--- cart.sh
+++ cart.sh
@@ -1,31 +1,31 @@
 echo -e "\e[31mExecuting repo file\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tem/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
 
 echo -e "\e[31mInstalling NodeJS\e[0m"
-yum install nodejs -y  &>>/tem/roboshop.log
+yum install nodejs -y  &>>/tmp/roboshop.log
 
 echo -e "\e[31mAdding user\e[0m"
-useradd roboshop &>>/tem/roboshop.log
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[31mRemoving old app foulder and creating new\e[0m"
-rm -rf /app  &>>/tem/roboshop.log
-mkdir /app   &>>/tem/roboshop.log
+rm -rf /app  &>>/tmp/roboshop.log
+mkdir /app   &>>/tmp/roboshop.log
 
 echo -e "\e[31mDownload the application code to created app directory\e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip  &>>/tem/roboshop.log
-cd /app  &>>/tem/roboshop.log
-unzip /tmp/cart.zip &>>/tem/roboshop.log
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip  &>>/tmp/roboshop.log
+cd /app  &>>/tmp/roboshop.log
+unzip /tmp/cart.zip &>>/tmp/roboshop.log
 
 echo -e "\e[31mAdding dependencies for nodejs\e[0m"
 cd /app
-npm install &>>/tem/roboshop.log
+npm install &>>/tmp/roboshop.log
 
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tem/roboshop.log
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
 
 
 
-systemctl daemon-reload &>>/tem/roboshop.log
+systemctl daemon-reload &>>/tmp/roboshop.log
 
 echo -e "\e[31Strating cart\e[0m"
-systemctl enable cart &>>/tem/roboshop.log
-systemctl restart cart &>>/tem/roboshop.log
+systemctl enable cart &>>/tmp/roboshop.log
+systemctl restart cart &>>/tmp/roboshop.log
