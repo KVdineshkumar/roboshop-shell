20acf5b5cf8469348d25a530abafe391e54e6851
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-01 17:31:23 +0530

    mongo db configuration is done here 2

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,15 @@
 # To add the repo file in manual process we use VIM  but Automation we creating a file server-name.repo(mongodb.repo) and add the configuration
 # BY using CP we copying the configuration giving a file name as shown in below .
 echo -e "\e[33mCopy MongoDB repo \e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tpm/roboshop.log
+cp mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstalling MongoDB server \e[0m"
-yum install mongodb-org -y &>>/tpm/roboshop.log
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 # here we want Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 echo -e "\e[34mUpdate listen address \e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /ect/mongodb.conf &>>/tpm/roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /ect/mongodb.conf &>>/tmp/roboshop.log
 
 echo -e "\e[35mStart  MongoDB server \e[0m"
-systemctl enable mongod &>>/tpm/roboshop.log
-systemctl restart mongod &>>/tpm/roboshop.log
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
