6e36e676c43b7caac81a6bc22674bbd7a7e7c9d2
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-01 16:25:41 +0530

    mongo db configuration is done here 2

--- mongodb.sh
+++ mongodb.sh
@@ -3,12 +3,12 @@
 cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tem/roboshop.log
 
 eco -e "\e[33mInstalling MongoDB server \e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tem/roboshop.log
 
 # here we want Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 eco -e "\e[34mUpdate listen address \e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /ect/mongodb.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /ect/mongodb.conf &>>/tem/roboshop.log
 
 eco -e "\e[35mStart  MongoDB server \e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tem/roboshop.log
+systemctl restart mongod &>>/tem/roboshop.log
