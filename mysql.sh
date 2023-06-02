2c3029ce1f4d11b68d308d6ea17aa02290f690db
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-02 09:29:14 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,20 @@
+# CentOS-8 Comes with MySQL 8 Version by default, However our application needs MySQL 5.7. So lets disable MySQL 8 version.
+echo -e "\e[31mInstalling repo file\e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+
+echo -e "\e[32mInstalling MySQL server\e[0m"
+yum install mysql-community-server -y &>>/tmp/roboshop.log
+
+echo -e "\e[31mStarting MySQL server\e[0m"
+systemctl enable mysqld
+systemctl start mysqld  &>>/tmp/roboshop.log
+
+echo -e "\e[32mchange the default root password \e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1  &>>/tmp/roboshop.log
+
+
+
+
+
+
+
