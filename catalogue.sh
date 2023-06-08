ab8293a9fde3a482c302f34f3b3981c11918bc14
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-08 09:44:42 +0530

    mysql

--- catalogue.sh
+++ catalogue.sh
@@ -1,38 +1,38 @@
+source common.sh
+
 component=catalogue
-color="\e[31m" # here we want to as \e[31m but it won't work becous "\" is an special charter we want denote by ""
-nocolor="\e[0m"
 echo -e "${color} THis is the script for repo${nocolor}"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
 echo -e "${color}Installing nodejs\e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
+yum install nodejs -y &>>${log_file}
 
 echo -e "${color}Adding User\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+useradd roboshop &>>${log_file}
 
 echo -e "${color}Creating a directory${nocolor}"
-rm -rf /app
-mkdir /app &>>/tmp/roboshop.log
+rm -rf ${app_path}
+mkdir ${app_path} &>>${log_file}
 
 echo -e "${color}Download the application code to created app directory${nocolor}"
-curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>/tmp/roboshop.log
-cd /app
-unzip /tmp/$component.zip &>>/tmp/roboshop.log
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>${log_file}
+cd ${app_path}
+unzip /tmp/$component.zip &>>${log_file}
 
 echo -e "${color}Installing nodejs dependencies${nocolor}"
 cd /app
-npm install &>>/tmp/roboshop.log
+npm install &>>${log_file}
 
-cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>${log_file}
 
 systemctl daemon-reload
 
 systemctl enable $component
-systemctl start $component &>>/tmp/roboshop.log
+systemctl start $component &>>${log_file}
 
 echo -e "${color}install mongodb-client${nocolor}"
-cp  /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
-yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+cp  /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+yum install mongodb-org-shell -y &>>${log_file}
 
 echo -e "${color}Load Schema${nocolor}"
-mongo --host mongodb-dev.devopsd73.store </app/schema/$component.js &>>/tmp/roboshop.log
+mongo --host mongodb-dev.devopsd73.store </app/schema/$component.js &>>${log_file}
---
+++ common.sh
@@ -0,0 +1,4 @@
+color="\e[31m" # here we want to as \e[31m but it won't work becous "\" is an special charter we want denote by ""
+nocolor="\e[0m"
+log_file="/tmp/roboshop.log"
+app_path="/app"
