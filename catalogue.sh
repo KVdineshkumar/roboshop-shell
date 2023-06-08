d2e3b1113b4c4e9ef3fc5a7f2031f4b1662f996b
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-08 10:31:11 +0530

    mysql

--- catalogue.sh
+++ catalogue.sh
@@ -2,9 +2,4 @@ source common.sh
 
 component=catalogue
 nodeJS
-echo -e "${color}install mongodb-client${nocolor}"
-cp  /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-yum install mongodb-org-shell -y &>>${log_file}
-
-echo -e "${color}Load Schema${nocolor}"
-mongo --host mongodb-dev.devopsd73.store </app/schema/$component.js &>>${log_file}
+mongo_shema_setup
