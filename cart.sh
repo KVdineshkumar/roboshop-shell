59a8fc37c10ca0e28336490d36dfcad72a4fc087
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-08 10:01:03 +0530

    mysql

--- cart.sh
+++ cart.sh
@@ -1,29 +1,31 @@
-echo -e "\e[31mExecuting repo file\e[0m"
+sorce common.sh
+component=cart
+echo -e "${color}Executing repo file ${nocolor}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
 
-echo -e "\e[31mInstalling NodeJS\e[0m"
+echo -e "${color}Installing NodeJS${nocolor}"
 yum install nodejs -y  &>>/tmp/roboshop.log
 
-echo -e "\e[31mAdding user\e[0m"
+echo -e "${color}Adding user${nocolor}"
 useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[31mRemoving old app foulder and creating new\e[0m"
-rm -rf /app  &>>/tmp/roboshop.log
-mkdir /app   &>>/tmp/roboshop.log
+echo -e "${color}Removing old app foulder and creating new${nocolor}"
+rm -rf ${app_apth}  &>>/tmp/roboshop.log
+mkdir ${app_apth}    &>>/tmp/roboshop.log
 
-echo -e "\e[31mDownload the application code to created app directory\e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip  &>>/tmp/roboshop.log
+echo -e "${color}Download the application code to created app directory${nocolor}"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>/tmp/roboshop.log
 cd /app  &>>/tmp/roboshop.log
-unzip /tmp/cart.zip &>>/tmp/roboshop.log
+unzip /tmp/${component}.zip &>>/tmp/roboshop.log
 
-echo -e "\e[31mAdding dependencies for nodejs\e[0m"
-cd /app
+echo -e "${color}Adding dependencies for nodejs${nocolor}"
+cd ${app_apth}
 npm install  &>>/tmp/roboshop.log
 
-echo -e "\e[31mSetup Systemd Service\e[0m"
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service  &>>/tmp/roboshop.log
+echo -e "${color}Setup Systemd Service${nocolor}"
+cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service  &>>/tmp/roboshop.log
 
-echo -e "\e[31mStrating cart server\e[0m"
+echo -e "${color}Strating cart server${nocolor}"
 systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable cart &>>/tmp/roboshop.log
-systemctl start cart &>>/tmp/roboshop.log
+systemctl enable ${component} &>>/tmp/roboshop.log
+systemctl start ${component} &>>/tmp/roboshop.log
