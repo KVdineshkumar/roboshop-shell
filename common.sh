b7017ee99326f745310b1cb010f5f8a48823f599
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-08 21:31:04 +0530

    mysql

--- common.sh
+++ common.sh
@@ -7,7 +7,7 @@ app_presetup () {
     echo -e "${color}Adding User\e[0m"
     useradd roboshop &>>${log_file}
     if [ $? -eq 1 ]; then
-      useradd roboshop
+      useradd roboshop1
       echo sucess
     else
       echo failure
