a5b7d9e10f57e96b282bb2dc6d46d77dc56a1445
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-08 09:46:28 +0530

    mysql

--- common.sh
+++ common.sh
@@ -1,4 +1,4 @@
-color="\e[31m" # here we want to as \e[31m but it won't work becous "\" is an special charter we want denote by ""
+color="\e[33m" # here we want to as \e[31m but it won't work becous "\" is an special charter we want denote by ""
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
