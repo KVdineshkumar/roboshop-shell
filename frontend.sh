36c5eb12ee88da9bbbc99e5ae3df0fa3b89c5193
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-09 13:49:49 +0530

    mysql

--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,23 @@
 source common.sh
-echo -e "\e[33minstalling nginx server roboshop\e[0m"
-yum install nginx -y &>>/tmp/roboshop.log
+echo -e "${color}installing nginx server roboshop${nocolor}"
+yum install nginx -y &>>${log_file}
+stat_check &>>${log_file}
+
+echo -e "${color}Here we are removing old config to config new${nocolor}"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
 stat_check
 
-echo -e "\e[34mHere we are removing old config to config new\e[0m"
-rm -rf /usr/share/nginx/html/*
+echo -e "${color}Downloading frontend UI code content${nocolor}"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${log_file}
 stat_check
 
-echo -e "\e[35mDownloading frontend UI code content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "${color}Here we are unzing the zip file${nocolor}"
+cd /usr/share/nginx/html &>>${log_file}
+unzip /tmp/frontend.zip &>>${log_file}
 stat_check
 
-echo -e "\e[36mHere we are unzing the zip file\e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-stat_check
-
-echo -e "\e[37mUpdating Frontend Configuration\e[0m"
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "${color}Updating Frontend Configuration${nocolor}"
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 systemctl enable nginx
-systemctl restart nginx
+systemctl restart nginx &>>${log_file}
 stat_check
